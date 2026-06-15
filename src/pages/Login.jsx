56152bd5d708c30530899118bf1eca1d04cc796a
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-06-15 23:18:38 +0530

    fix: add missing line break in Login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
           <div className="flex flex-col gap-2 mt-4 border-2 bg-amber-50">
             <label htmlFor="email">Email:</label>
             <input type="text" id="email" placeholder="enter your email" />
+            
           </div>
         </form>
       </div>
